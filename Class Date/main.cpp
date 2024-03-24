b9d900a810aae5389281f25ae36ab30cd91cc386
Aizek <129169524+AItEKS@users.noreply.github.com>
2024-03-25 00:49:11 +0800

    Update main.cpp

--- Class Date/main.cpp
+++ Class Date/main.cpp
@@ -7,10 +7,13 @@ int main()
 
     Date a(29, 2, 2020);
     Date b("12.12.2023");
+    Date c(25, "Март", 2024);
 
     std::cout << "Первая дата: " << a << std::endl;
     std::cout << "Вторая дата: " << b << std::endl;
     std::cout << "Разница между датами: " << a - b << std::endl;
+
     std::cout << "День недели первой даты: " << a.weekDay() << std::endl;
     std::cout << "День недели второй даты: " << b.weekDay() << std::endl;
+    std::cout << "День недели третьей даты: " << c.weekDay() << std::endl;
 }
