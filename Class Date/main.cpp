448a138580233fa89f365e66bbda8240601db0eb
Aizek <129169524+AItEKS@users.noreply.github.com>
2024-03-24 22:14:19 +0800

    Create main.cpp

---
+++ Class Date/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Date.h"
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    Date a(29, 2, 2020);
+    Date b("12.12.2023");
+
+    std::cout << "Первая дата: " << a << std::endl;
+    std::cout << "Вторая дата: " << b << std::endl;
+    std::cout << "Разница между датами: " << a - b << std::endl;
+    std::cout << "День недели первой даты: " << a.weekDay() << std::endl;
+    std::cout << "День недели второй даты: " << b.weekDay() << std::endl;
+}
