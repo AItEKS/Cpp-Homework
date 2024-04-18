24494c5f3e9e27c4f57c70b6d5cc15fb07adfc1a
Aizek <129169524+AItEKS@users.noreply.github.com>
2024-04-18 15:09:29 +0800

    Update Graph.cpp

--- Class Graph/Graph.cpp
+++ Class Graph/Graph.cpp
@@ -34,3 +34,24 @@ std::set<Node*>::const_iterator Graph::begin() const {
 std::set<Node*>::const_iterator Graph::end() const {
     return nodes.end();
 }
+
+Node* Graph::binarySearch(const std::string& name) const {
+    int left = 0;
+    int right = nodes.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        std::set<Node*>::const_iterator it = nodes.begin();
+        std::advance(it, mid);
+        const Node* midNode = *it;
+        if (midNode->getName() == name) {
+            return const_cast<Node*>(midNode);
+        }
+        else if (midNode->getName() < name) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return nullptr;
+}
